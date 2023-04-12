824f60990c0994bc750e457b2cdd0ed55ead5403
Majd Barakat <54665027+majdbrt@users.noreply.github.com>
2023-04-12 13:33:16 -0500

    Update socket.js

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = process.env.REACT_APP_API_URL;
+const URL = "https://droplet-39g3.onrender.com";
 
 export const socket = io(URL, {
  autoConnect:false
