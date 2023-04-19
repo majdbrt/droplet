b70f7d24ba2f237e1707c82ef5daeb471723b952
Majd Barakat <54665027+majdbrt@users.noreply.github.com>
2023-04-19 09:27:51 -0500

    Update LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -128,18 +128,18 @@ function LoginForm() {
     }// handleSubmit
 
     return (
-        <div className="flex-1 flex flex-col items-center overflow-x-hidden h-full w-full " >
+        <div className=" flex flex-col self-center my-auto items-center overflow-x-hidden  w-full  " >
 
-            <form className=" flex-1 mx-auto py-12  px-2 md:p-20 md:pt-14 md:pb-12 rounded-lg h-auto" onSubmit={handleSubmit}>
+            <form className="  w-[80%] pt-3 pb-10 px-3 md:p-20 md:pt-3 md:pb-12 rounded-lg h-auto" onSubmit={handleSubmit}>
                 <div className="relative md:m-10  mb-0 md:mb-0 md:mt-5" >
-                    <input autoComplete="off" id="login-email" name="email" type="email" onChange={handleChange} onBlur={validateEmail} className={`block px-3  ${darkTheme ? ' text-white bg-neutral-900 hover:bg-neutral-800 focus:bg-neutral-800 ' : 'text-dark  hover:bg-neutral-50 focus:bg-neutral-50'} focus:outline-none border-solid appearance-none peer/email py-4 rounded-lg focus:border-cyan-400 hover:border-cyan-400 outline-none ${valid.email ? 'border-neutral-500' : 'border-rose-500'}  focus:outline border-2 w-[95%] mx-auto md:w-96`} placeholder="" value={form.email} />
+                    <input autoComplete="off" id="login-email" name="email" type="email" onChange={handleChange} onBlur={validateEmail} className={`block px-3  ${darkTheme ? ' text-white bg-neutral-900 hover:bg-neutral-800 focus:bg-neutral-800 ' : 'text-dark  hover:bg-neutral-50 focus:bg-neutral-50'} focus:outline-none border-solid appearance-none peer/email py-4 rounded-lg focus:border-cyan-400 hover:border-cyan-400 outline-none ${valid.email ? 'border-neutral-500' : 'border-rose-500'}  focus:outline border-2 w-full mx-auto md:w-96`} placeholder="" value={form.email} />
 
                     <label htmlFor="login-email" className={`absolute filled ${darkTheme ? 'bg-neutral-900' : 'bg-white'}  ${form.email ? '-translate-y-7 text-sm ' : 'text-lg'}  top-4 left-3 ${valid.email ? 'text-neutral-500' : 'text-rose-500'} peer-focus/email:-translate-y-7 peer-hover/email:-translate-y-7 transform peer-focus/email:text-cyan-400 peer-hover/email:text-cyan-400 peer-focus/email:text-sm peer-hover/email:text-sm  z-10`}>Email address</label>
                     <p className={`${valid.email ? 'invisible' : ''} text-rose-500 pt-2 text-left`}>Please enter a valid email address</p>
                 </div>
 
                 <div className="relative md:m-10 md:my-3 my-3" >
-                    <input autoComplete="off" id="login-password" name="password" type={`${form.showPassword ? 'text' : 'password'}`} onChange={handleChange} onBlur={validatePassword} className={`block px-3  ${darkTheme ? ' text-white bg-neutral-900 hover:bg-neutral-800 focus:bg-neutral-800 ' : 'text-dark  hover:bg-neutral-50 focus:bg-neutral-50'} focus:outline-none border-solid appearance-none peer/email py-4 rounded-lg focus:border-cyan-400 hover:border-cyan-400 outline-none ${valid.password ? 'border-neutral-500' : 'border-rose-500'}  focus:outline border-2 w-[95%] mx-auto md:w-96`} placeholder="" value={form.password} />
+                    <input autoComplete="off" id="login-password" name="password" type={`${form.showPassword ? 'text' : 'password'}`} onChange={handleChange} onBlur={validatePassword} className={`block px-3  ${darkTheme ? ' text-white bg-neutral-900 hover:bg-neutral-800 focus:bg-neutral-800 ' : 'text-dark  hover:bg-neutral-50 focus:bg-neutral-50'} focus:outline-none border-solid appearance-none peer/email py-4 rounded-lg focus:border-cyan-400 hover:border-cyan-400 outline-none ${valid.password ? 'border-neutral-500' : 'border-rose-500'}  focus:outline border-2 w-full mx-auto md:w-96`} placeholder="" value={form.password} />
                     <img src={eyeIcon} alt="" onClick={handleShowPassword} className="scale-75 absolute right-3 top-5" />
                     <label htmlFor="login-passowrd" className={`absolute filled ${darkTheme ? 'bg-neutral-900' : 'bg-white'}  ${form.password ? '-translate-y-7 text-sm ' : 'text-lg'}  top-4 left-3 ${valid.password ? 'text-neutral-500' : 'text-rose-500'} peer-focus/email:-translate-y-7 peer-hover/email:-translate-y-7 transform peer-focus/email:text-cyan-400 peer-hover/email:text-cyan-400 peer-focus/email:text-sm peer-hover/email:text-sm  z-10`}>Password</label>
                     <p className={`${valid.password ? 'invisible' : ''} text-rose-500 pt-2 text-left`}>Please enter a valid password</p>
@@ -162,10 +162,10 @@ function LoginForm() {
 
             </form>
 
-            <hr className=" border-neutral-500 w-80 md:w-96 "></hr>
+            <hr className=" border-neutral-500 w-[80%] md:w-96 "></hr>
             <p className={` ${darkTheme ? 'text-white' : 'text-black'}  mt-10 mb-7 font-md text-xl`}>Don't have an account?</p>
 
-            <div className={`mb-10 ${darkTheme ? 'bg-neutral-900 hover:border-white' : 'bg-white  hover:border-black'} font-medium text-xl text-neutral-400 scale-90   hover:scale-95 border-2 border-neutral-500  rounded-full`}>
+            <div className={`w-[80%] mb-10 ${darkTheme ? 'bg-neutral-900 hover:border-white' : 'bg-white  hover:border-black'} font-medium text-xl text-neutral-400 scale-90   hover:scale-95 border-2 border-neutral-500  rounded-full`}>
                 <button className="cursor-default px-14  md:px-28 py-3 md:mx-2" type="button" onClick={()=>navigate("/signup")}>Sign up for droplet</button>
             </div>
         </div>
