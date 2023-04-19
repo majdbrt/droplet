4c911171af7c141dc89f15e2e1497e41bf8f5e42
Majd Barakat <54665027+majdbrt@users.noreply.github.com>
2023-04-19 14:45:45 -0500

    Update Error.js

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,15 +1,15 @@
 
 import errorIcon from "../images/error-icon.png";
+import { useSelector } from "react-redux";
 
 function Error(props){
-
+    const darkTheme = useSelector((state) => state.user.darkTheme);
     return(
-        <div className="flex w-[90%] h-16 bg-transparent  text-rose-500 border-rose-500 border-2">
+        <div className={`flex w-[90%] h-16 ${darkTheme? 'bg-neutral-700' : 'bg-neutral-100'} bg-neutral-700  text-rose-500 border-rose-500 border-2`}>
             <img className="w-8 h-8 my-auto mx-3 " src={errorIcon} alt=""/>
             <p className="my-auto ">{props.text}</p>
         </div>
     );
 }
-
 
 export default Error
